8427557c70dd462a3e0f3bfd498dbb39cf85c9e7
max <maxim_ovidiu_alex@yahoo.com>
2023-04-27 21:40:15 +0300

    - revert to initial state

--- src/main/java/max/demo/springbootkeycloak/config/WebSecurityConfig.java
+++ src/main/java/max/demo/springbootkeycloak/config/WebSecurityConfig.java
@@ -3,13 +3,8 @@ package max.demo.springbootkeycloak.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 /**
  * don't use KeycloakWebSecurityConfigurerAdapter
@@ -20,27 +15,9 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
-
     @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                .requestMatchers("/home").hasRole("hogwarts_resident")
-                // the problem is that instead of looking for roles is looking for granted authorities
-                // see -> AuthorityAuthorizationManager -> private boolean isAuthorized(Authentication authentication)
-                .anyRequest().authenticated());
-        http.oauth2Login()
-                .and()
-                .logout()
-                .addLogoutHandler(keycloakLogoutHandler)
-                .logoutSuccessUrl("/");
-        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-        return http.build();
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().anyRequest();
     }
 
 }
--- src/main/java/max/demo/springbootkeycloak/controller/TestsController.java
+++ src/main/java/max/demo/springbootkeycloak/controller/TestsController.java
@@ -3,19 +3,12 @@ package max.demo.springbootkeycloak.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class TestsController {
 
-    @GetMapping("/tests-dates")
-    public List<String> getTests() {
-        return List.of("25.09.2023", "25.12.2023", "25.06.2024");
-    }
-
-    @GetMapping("/surprise-tests-dates")
-    public List<String> getSurpriseTests() {
-        return List.of("06.10.2023", "10.12.2023", "13.03.2024");
+    @GetMapping("/admin")
+    public String getAdminText() {
+        return "hello admin";
     }
 
 }
