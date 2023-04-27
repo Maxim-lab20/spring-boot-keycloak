85876660b6e72b592d065a8702039702f7b4915c
max <maxim_ovidiu_alex@yahoo.com>
2023-04-27 22:04:13 +0300

    - authentication works based on scopes

--- src/main/java/max/demo/springbootkeycloak/config/WebSecurityConfig.java
+++ src/main/java/max/demo/springbootkeycloak/config/WebSecurityConfig.java
@@ -3,8 +3,13 @@ package max.demo.springbootkeycloak.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 /**
  * don't use KeycloakWebSecurityConfigurerAdapter
@@ -15,9 +20,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().anyRequest();
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/admin*")
+                .hasAuthority("SCOPE_email")
+                .anyRequest()
+                .permitAll();
+        http.oauth2Login()
+                .and()
+                .logout()
+                .addLogoutHandler(keycloakLogoutHandler)
+                .logoutSuccessUrl("/");
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
     }
 
 }
--- src/main/java/max/demo/springbootkeycloak/controller/TestsController.java
+++ src/main/java/max/demo/springbootkeycloak/controller/TestsController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestsController {
 
+    @GetMapping("/home")
+    public String getHomeText() {
+        return "welcome home";
+    }
+
     @GetMapping("/admin")
     public String getAdminText() {
         return "hello admin";
